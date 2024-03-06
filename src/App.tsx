793c72ac707c9a341f77771beb80f69bd818e054
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-03-06 13:46:48 -0700

    Make it work dangit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+const bandNames = [
+  {
+    name: 'Rush',
+    members: ['Geddy Lee', 'Alex Lifeson', 'Neil Peart'],
+    formed: 1972,
+  },
+  {
+    name: 'Led Zeppelin',
+    members: ['Robert Plant', 'Jimmy Page', 'John Paul Jones', 'John Bonham'],
+    formed: 1968,
+  },
+  {
+    name: 'Duran Duran',
+    members: [
+      'Simon Le Bon',
+      'Nick Rhodes',
+      'John Taylor',
+      'Roger Taylor',
+      'Andy Taylor',
+    ],
+    formed: 1978,
+  },
+  {
+    name: 'Nirvana',
+    members: ['Kurt Cobain', 'Krist Novoselic', 'Dave Grohl'],
+    formed: 1987,
+  },
+];
 
-function App() {
+function Welcome() {
+  return <h1>The Best Bands</h1>;
+}
 
-  
+type BandProps = {
+  name: string;
+  members: string[];
+  formed: number;
+};
 
+class Band extends React.Component<BandProps> {
+  render() {
+    const { name, members, formed } = this.props;
+
+    return (
+      <div>
+        <img />
+        <h2>{name}</h2>
+        <h3>Members: {members}</h3>
+        <h3>Formed: {formed}</h3>
+      </div>
+    );
+  }
+}
+
+function BandList() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-
-        </a>
-      </header>
+    <div>
+      {bandNames.map((bandNum) => (
+        <Band {...bandNum} />
+      ))}
     </div>
   );
 }
 
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <BandList />
+    </div>
+  );
+}
 export default App;
